9f6e0e1deea4527741b0de30e0bb2db3729a0003
MontseAguilarR <89093129+MontseAguilarR@users.noreply.github.com>
2021-10-09 19:59:54 -0500

    Add files via upload

---
+++ src/uaslp/ingenieria/exams/Instrumentos.java
@@ -0,0 +1,15 @@
+package uaslp.ingenieria.exams;
+
+public abstract class Instrumentos {
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void tocar(Nota nota) {
+
+    }
+
+}
