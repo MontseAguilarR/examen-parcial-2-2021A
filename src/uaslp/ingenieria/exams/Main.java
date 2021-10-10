750ea0ef0d74fb699b286115db387de6647afe45
MontseAguilarR <89093129+MontseAguilarR@users.noreply.github.com>
2021-10-09 19:59:14 -0500

    Update Main.java

--- src/uaslp/ingenieria/exams/Main.java
+++ src/uaslp/ingenieria/exams/Main.java
@@ -1,19 +1,31 @@
 package uaslp.ingenieria.exams;
 
-import uaslp.ingenieria.exams.instruments.Instrumento;
+import uaslp.ingenieria.exams.instruments.*;
 
 import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         String fileName = "archivo.music"; //args[0];
-        Instrumento instrumento = Instrumento.valueOf("PIANO");  //args[1]
+        List<Instrumentos> listaInstrumentos=new LinkedList<>();
+
+        Piano piano = new Piano();
+        Guitarra guitarra = new Guitarra();
+        Violin violin = new Violin();
+        Flauta flauta = new Flauta();
+
+        listaInstrumentos.add(piano);
+        listaInstrumentos.add(guitarra);
+        listaInstrumentos.add(violin);
+        listaInstrumentos.add(flauta);
 
         SongPlayer songPlayer = new SongPlayer();
         Song song = new Song(new File(fileName));
 
-        songPlayer.play(song, instrumento);
+        songPlayer.play(song, listaInstrumentos);
 
     }
 }
