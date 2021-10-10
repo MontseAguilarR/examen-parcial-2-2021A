c5948c84012aba79b6bc4e19eea5a475f6611b09
MontseAguilarR <89093129+MontseAguilarR@users.noreply.github.com>
2021-10-09 20:00:43 -0500

    Update Flauta.java

--- src/uaslp/ingenieria/exams/instruments/Flauta.java
+++ src/uaslp/ingenieria/exams/instruments/Flauta.java
@@ -1,9 +1,15 @@
 package uaslp.ingenieria.exams.instruments;
 
+import uaslp.ingenieria.exams.Instrumentos;
 import uaslp.ingenieria.exams.Nota;
 
-public class Flauta {
+public class Flauta extends Instrumentos {
+
     public void sopla(Nota nota) {
 
+    }
+
+    public void tocar(Nota nota) {
+        
     }
 }
