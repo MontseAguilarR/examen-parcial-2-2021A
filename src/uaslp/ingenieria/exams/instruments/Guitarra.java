f7fdde5be9e2c06820063f890fe75c2670a29e35
MontseAguilarR <89093129+MontseAguilarR@users.noreply.github.com>
2021-10-09 20:01:18 -0500

    Update Guitarra.java

--- src/uaslp/ingenieria/exams/instruments/Guitarra.java
+++ src/uaslp/ingenieria/exams/instruments/Guitarra.java
@@ -1,10 +1,16 @@
 package uaslp.ingenieria.exams.instruments;
 
+import uaslp.ingenieria.exams.Instrumentos;
 import uaslp.ingenieria.exams.Nota;
 
-public class Guitarra {
+public class Guitarra extends Instrumentos {
     public void tocaCuerda(Nota nota) {
 
+
+    }
+
+    @Override
+    public void tocar(Nota nota) {
 
     }
 }
